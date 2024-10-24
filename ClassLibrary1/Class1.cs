034fe1d0642cdea24d022ef7700b9691e4e25899
Simon Cropp <simon.cropp@gmail.com>
2024-10-25 05:10:36 +1100

    .

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ClassLibrary1;
+
+public class Class1
+{
+    public static void Method<T>([UnscopedRef] params ReadOnlySpan<T> args)
+    {
+    }
+}
