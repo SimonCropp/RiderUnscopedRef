e696c3eb338799dac952072996e4c212d698d9eb
Simon Cropp <simon.cropp@gmail.com>
2024-10-25 05:18:46 +1100

    .

---
+++ ClassLibrary1/UnscopedRefAttribute.cs
@@ -0,0 +1,12 @@
+﻿#if !NET7_0_OR_GREATER
+
+namespace System.Diagnostics.CodeAnalysis;
+
+[AttributeUsage(
+    validOn: AttributeTargets.Method |
+             AttributeTargets.Property |
+             AttributeTargets.Parameter,
+    Inherited = false)]
+sealed class UnscopedRefAttribute :
+    Attribute;
+#endif
